560bda0fcdeaf9ddf8af80bca7df1ade6b55053d
Sanja Abbott <sanja@pdbe-onedep-emdb.ebi.ac.uk>
2019-11-06 14:42:43 +0000

    changed schema version to v3.0.1.7

--- wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
+++ wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
@@ -1,5 +1,5 @@
 # file: generatedsnamespaces.py
 
 GenerateDSNamespaceDefs = {
-    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas/blob/master/v3/v3_0_1_5/emdb.xsd"',
+    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas/blob/master/v3/v3_0_1_7/emdb.xsd"',
 }
