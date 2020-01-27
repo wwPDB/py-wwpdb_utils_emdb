c36d06d8c48ec8cc07b6eb098e2e5b4b0a987907
Sanja Abbott <sanja@emdb-1.ebi.ac.uk>
2020-01-27 17:47:18 +0000

    changes for new schema v3.0.1.8

--- wwpdb/utils/emdb/cif_emdb_translator/cif_emdb_translator.py
+++ wwpdb/utils/emdb/cif_emdb_translator/cif_emdb_translator.py
@@ -100,7 +100,7 @@ class CifEMDBTranslator(object):
         There are many constants in use for the translation.
         They have been collected here for ease of use.
         """
-        XML_OUT_VERSION = '3.0.1.7'
+        XML_OUT_VERSION = '3.0.1.8'
 
         # Cif categories
         CITATION = 'citation'
