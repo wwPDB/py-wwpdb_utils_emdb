2cc9c36fb22b93ceaa290bab71c6613506e3d5a4
sanja <sanja@ebi.ac.uk>
2021-04-08 17:30:16 +0100

    Changes for schema v3.0.2.5 - again

--- wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
+++ wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
@@ -1,9 +1,5 @@
 # file: generatedsnamespaces.py
 
 GenerateDSNamespaceDefs = {
-<<<<<<< HEAD
     "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas/blob/master/v3/v3_0_2_5/emdb.xsd"',
-=======
-    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas/blob/master/v3/v3_0_2_4/emdb.xsd"',
->>>>>>> origin/develop
 }
