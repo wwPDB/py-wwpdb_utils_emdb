e878119ea92dcee4dd91df7bdc883acf23c53ebb
amudhakumari <52195626+amudhakumari@users.noreply.github.com>
2022-11-04 13:53:45 +0000

    Daother 7669 (#35)
    * Namespace change from emd to em
    
    * fixed bugs mentioned in log
    
    * Test file changed to em namespace
    
    * changed mock-data/EMD/emd-0000.cif
    
    * removed the conflicts
    
    * corrected const name
    
    * corrected long text
    
    * wwpdb/mock-data/EMD/emd-0000.cif
    
    * changed emd to em test file
    
    * emd to em-test data
    
    * emd to em-test data
    
    * test coverage reduced to 34
    
    * checking mmcif dict
    
    * checking mmcif dict
    
    * checking mmcif dict
    
    * checking mmcif dict(3)
    
    * reformating issues
    
    * Fixed identifier_ORCID
    
    * removed pass
    
    * all emd to em checked

--- wwpdb/utils/emdb/cif_emdb_translator/test_cif_to_xml_translator.py
+++ wwpdb/utils/emdb/cif_emdb_translator/test_cif_to_xml_translator.py
@@ -24,7 +24,7 @@ def main():
 
             Examples:
             python test_cif_to_xml_translator.py -i input_cif_file -o output_xml_file
-            python test_cif_to_xml_translator.py -i "data/cif/EMD-0000.cif" -o "data/test/xml_v3_out/EMD-0000.xml"
+            python test_cif_to_xml_translator.py -i "data/cif/EMD-0000.txt" -o "data/test/xml_v3_out/EMD-0000.xml"
 
             Typical run:
             python test_cif_to_xml_translator.py -i in.cif -o out.xml
@@ -43,7 +43,7 @@ def main():
     if options.in_cif:
         in_cif = options.in_cif
     else:
-        in_cif = "data/cif/EMD-0000.cif"
+        in_cif = "data/cif/EMD-0000.txt"
     if options.out_xml:
         out_xml = options.out_xml
     else:
