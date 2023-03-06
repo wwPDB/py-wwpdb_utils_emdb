fc1640f4233566197c8ef985b70501e15b3787f0
amudha <amudha@ebi.ac.uk>
2023-03-06 09:23:53 +0000

    changes in generatedsnamespaces.py

--- wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
+++ wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
@@ -1,5 +1,8 @@
 # file: generatedsnamespaces.py
 
 GenerateDSNamespaceDefs = {
-    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas/blob/master/v3/v3_0_3_0/emdb.xsd"',
+    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' 
+                  'xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas'
+                  '/blob/master/emdb_schemas/v3/v3_0_3_0/emdb.xsd"'
 }
+GenerateDSNamespaceTypePrefixes = {}
