135af7f2360359d7da1c7d3f013e7e5740d6e380
amudha <amudha@ebi.ac.uk>
2023-04-25 11:46:11 +0100

    float and version fixed

--- wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
+++ wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
@@ -1,8 +1,10 @@
 # file: generatedsnamespaces.py
 
 GenerateDSNamespaceDefs = {
-    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '  
+    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
                   'xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas'
-                  '/blob/master/emdb_schemas/v3/v3_0_3_2/emdb.xsd"'
+                  '/blob/master/emdb_schemas/v3/v3_0_3_2/emdb.xsd" '
+                  'version="3.0.3.2"'
 }
 GenerateDSNamespaceTypePrefixes = {}
+
