133c6a9c6b782d1b0f6122d4a51dbaec477ef3f1
amudha <amudha@ebi.ac.uk>
2023-05-03 15:06:55 +0100

    changed to TFS FALCON 4i (4k x 4k)

--- wwpdb/utils/emdb/cif_emdb_translator/cif_emdb_translator.py
+++ wwpdb/utils/emdb/cif_emdb_translator/cif_emdb_translator.py
@@ -106,7 +106,7 @@ class CifEMDBTranslator(object):
         They have been collected here for ease of use.
         """
 
-        XML_OUT_VERSION = "3.0.3.2"
+        XML_OUT_VERSION = "3.0.3.3"
 
         # Cif categories
         CITATION = "citation"
--- wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
+++ wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
@@ -3,6 +3,7 @@
 GenerateDSNamespaceDefs = {
     "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '  
                   'xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas'
-                  '/blob/master/emdb_schemas/v3/v3_0_3_2/emdb.xsd"'
+                  '/blob/master/emdb_schemas/v3/v3_0_3_3/emdb.xsd" '
+                  'version="3.0.3.3"'
 }
 GenerateDSNamespaceTypePrefixes = {}
