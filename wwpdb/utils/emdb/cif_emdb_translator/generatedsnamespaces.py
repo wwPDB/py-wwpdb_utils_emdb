6d9de6f4722047fb63d0bd5c769d5e701f4ea08f
amudha <amudha@ebi.ac.uk>
2023-05-30 16:34:55 +0100

    noNS added

--- wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
+++ wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
@@ -2,7 +2,7 @@
 
 GenerateDSNamespaceDefs = {
     "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
-                  'xsi:schemaLocation="https://ftp.ebi.ac.uk/pub/databases/em_ebi/emdb_related/emdb-schemas/'
+                  'xsi:noNamespaceSchemaLocation="https://ftp.ebi.ac.uk/pub/databases/em_ebi/emdb_related/emdb-schemas/'
                   'emdb_schemas/v3/v3_0_4_1/emdb.xsd" '
                   'version="3.0.4.1"'
 }
