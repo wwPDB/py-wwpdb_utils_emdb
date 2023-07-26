517d929693ad4578d93d04055a164c4ac28adfce
amudha <amudha@ebi.ac.uk>
2023-07-26 17:11:18 +0100

    missing attributes fixed

--- wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
+++
@@ -1,9 +0,0 @@
-# file: generatedsnamespaces.py
-
-GenerateDSNamespaceDefs = {
-    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
-                  'xsi:noNamespaceSchemaLocation="https://ftp.ebi.ac.uk/pub/databases/em_ebi/emdb_related/emdb-schemas/'
-                  'emdb_schemas/v3/v3_0_7_2/emdb.xsd" '
-                  'version="3.0.7.2"'
-}
-GenerateDSNamespaceTypePrefixes = {}
