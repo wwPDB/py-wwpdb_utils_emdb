a8ef95542926c595d71ad49064bef3eb13867d42
lucasemdb <lucas@ebi.ac.uk>
2024-01-09 14:59:41 +0000

    Start test script for new workflow

--- wwpdb/utils/tests-emdb/emmapcheck/emmapcheckTests.py
+++ wwpdb/utils/tests-emdb/emmapcheck/emmapcheckTests.py
@@ -1,41 +1,29 @@
-# import unittest
-# from wwpdb.utils.emdb.emmapcheck.emmapcheck import EMMap, Validator, Model
+import os
+import django
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "wwpdb.apps.deposit.settings"
+django.setup()
+
+from wwpdb.apps.deposit.depui.FileConversion import FileConversion
+from wwpdb.apps.deposit.depui.common_functions import getFile
+from wwpdb.utils.wf.dbapi.WfDbApi import WfDbApi
+
+username = "D_800288"
+
+wf_op = "emmapcheck_dep"
+fc = FileConversion()
+wfApi = WfDbApi(verbose=True)
+status = fc.wfStart(username, wfApi, wf_op, "wf_op_emmapcheck_fs_tempdep.xml")
+wf_data = fc.wfStatus(username, wfApi)
+finished = fc.wfFinished(username, wfApi, wf_data, wf_op)
+
+if status == 0 or not finished:
+    print("Unable to perform checks on uploaded files!")
+
+path2output = getFile(username, "tempdep", "em-map-report", "json", versionId='latest', mileStone=None, wfInstanceId=None, partNumber=None)
 
 
-# class TestEMMap(unittest.TestCase):
-#     def setUp(self):
-#         # Initialize an EMMap instance for testing
-#         self.em_map = EMMap('path_to_test_map_file')
-
-#     def test_load(self):
-#         # Test the load method
-#         pass  # Replace with actual test
-
-#     def test_md5_checksum(self):
-#         # Test the md5_checksum method
-#         pass  # Replace with actual test
-
-#     # Add more tests for the other methods in the EMMap class
-
-# class TestModel(unittest.TestCase):
-#     def setUp(self):
-#         # Initialize a Model instance for testing
-#         self.model = Model('path_to_test_model_file')
-
-#     # Add tests for the methods in the Model class
-
-# class TestValidator(unittest.TestCase):
-#     def setUp(self):
-#         # Initialize a Validator instance for testing
-#         self.validator = Validator(EMMap('path_to_test_map_file'))
-
-#     def test_check(self):
-#         # Test the check method
-#         pass  # Replace with actual test
-
-#     # Add more tests for the other methods in the Validator class
-
-import sys
-if __name__ == '__main__':
-    # unittest.main()
-    sys.exit(0)
+# import sys
+# if __name__ == '__main__':
+#     # unittest.main()
+#     sys.exit(0)
