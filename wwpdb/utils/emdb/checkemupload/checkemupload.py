555deaa90e24cf8683ff082fe6aa712403b5ec60
Neli Fonseca <neli@ebi.ac.uk>
2024-04-25 19:09:09 +0100

    Fix check atoms outside the box

--- wwpdb/utils/emdb/checkemupload/checkemupload.py
+++ wwpdb/utils/emdb/checkemupload/checkemupload.py
@@ -229,15 +229,37 @@ class Validator:
         self.model = model
 
     def _map_matrix(self, apixs, angs):
-        # Convert angles to radians
-        angs = [math.radians(ang) for ang in angs]
-        cos_alpha, cos_beta, cos_gamma = [math.cos(ang) for ang in angs]
-        sin_alpha, sin_beta, sin_gamma = [math.sin(ang) for ang in angs]
-        matrix = np.array([
-            [apixs[0], apixs[1] * cos_gamma, apixs[2] * cos_beta],
-            [0, apixs[1] * sin_gamma, -apixs[2] * sin_beta * cos_alpha],
-            [0, 0, apixs[2] * sin_beta * sin_alpha]
-        ])
+        """
+        calculate the matrix to transform Cartesian coordinates to fractional coordinates
+        (check the definination to see the matrix formular)
+        :param apixs: array of apix lenght
+        :param angs: array of anglex in alpha, beta, gamma order
+        :return: array of matrix
+        """
+
+        ang = (angs[0] * math.pi / 180, angs[1] * math.pi / 180, angs[2] * math.pi / 180)
+        insidesqrt = 1 + 2 * math.cos(ang[0]) * math.cos(ang[1]) * math.cos(ang[2]) - \
+                     math.cos(ang[0]) ** 2 - \
+                     math.cos(ang[1]) ** 2 - \
+                     math.cos(ang[2]) ** 2
+
+        cellvolume = apixs[0] * apixs[1] * apixs[2] * math.sqrt(insidesqrt)
+
+        m11 = 1 / apixs[0]
+        m12 = -math.cos(ang[2]) / (apixs[0] * math.sin(ang[2]))
+
+        m13 = apixs[1] * apixs[2] * (math.cos(ang[0]) * math.cos(ang[2]) - math.cos(ang[1])) / (
+                    cellvolume * math.sin(ang[2]))
+        m21 = 0
+        m22 = 1 / (apixs[1] * math.sin(ang[2]))
+        m23 = apixs[0] * apixs[2] * (math.cos(ang[1]) * math.cos(ang[2]) - math.cos(ang[0])) / (
+                    cellvolume * math.sin(ang[2]))
+        m31 = 0
+        m32 = 0
+        m33 = apixs[0] * apixs[1] * math.sin(ang[2]) / cellvolume
+        prematrix = [[m11, m12, m13], [m21, m22, m23], [m31, m32, m33]]
+        matrix = np.asarray(prematrix)
+
         return matrix
 
     def _matrix_indices(self, apixs, onecoor):
@@ -247,12 +269,57 @@ class Validator:
         return result[0] - self.em_map.nstarts[0], result[1] - self.em_map.nstarts[1], result[2] - self.em_map.nstarts[
             2]
 
+    def header_check(self):
+        crs = (self.em_map.header.mapc, self.em_map.header.mapr, self.em_map.header.maps)
+        crsindices = (crs.index(1), crs.index(2), crs.index(3))
+        self.nstarts = (self.em_map.header.nxstart, self.em_map.header.nystart, self.em_map.header.nzstart)
+        self.nxyz = (self.em_map.header.nx, self.em_map.header.ny, self.em_map.header.nz)
+
+        if crs != (1, 2, 3):
+            self.nxyz = (self.nxyz[crsindices[0]], self.nxyz[crsindices[1]], self.nxyz[crsindices[2]])
+            self.nstarts = (self.nstarts[crsindices[0]], self.nstarts[crsindices[1]], self.nstarts[crsindices[2]])
+
     def _get_indices(self, onecoor):
-        apixs = [self.em_map.header.cella.x / self.em_map.nxyz[0],
-                 self.em_map.header.cella.y / self.em_map.nxyz[1],
-                 self.em_map.header.cella.z / self.em_map.nxyz[2]]
-        oneindex = self._matrix_indices(apixs, onecoor)
-        return oneindex, self.em_map.nxyz
+        """
+                Find one atom's indices correspoding to its cubic or plane
+                the 8 (cubic) or 4 (plane) indices are saved in indices variable
+            :param map: Density map instance from TEMPy.MapParser
+            :param onecoor: List contains the atom coordinates in (x, y, z) order
+            :return: Tuple contains two list of index: first has the 8 or 4 indices in the cubic;second has the float index of the input atom
+            """
+
+        zdim = self.em_map.header.cella.z
+        znintervals = self.em_map.header.mz
+        z_apix = zdim / znintervals
+
+        ydim = self.em_map.header.cella.y
+        ynintervals = self.em_map.header.my
+        y_apix = ydim / ynintervals
+
+        xdim = self.em_map.header.cella.x
+        xnintervals = self.em_map.header.mx
+        x_apix = xdim / xnintervals
+
+        self.header_check()
+        map_xsize, map_ysize, map_zsize = self.nxyz
+        nxstart, nystart, nzstart = self.nstarts
+
+        if self.em_map.header.cellb.alpha == self.em_map.header.cellb.beta == self.em_map.header.cellb.gamma == 90.:
+            crs = [self.em_map.header.mapc, self.em_map.header.mapr, self.em_map.header.maps]
+            ordinds = [crs.index(1), crs.index(2), crs.index(3)]
+
+            zindex = float(onecoor[2] - self.em_map.header.origin.z) / z_apix - nzstart
+            yindex = float(onecoor[1] - self.em_map.header.origin.y) / y_apix - nystart
+            xindex = float(onecoor[0] - self.em_map.header.origin.x) / x_apix - nxstart
+
+
+        else:
+            apixs = [x_apix, y_apix, z_apix]
+            xindex, yindex, zindex = self._matrix_indices(apixs, onecoor)
+
+        oneindex = [xindex, yindex, zindex]
+
+        return oneindex, self.nxyz
 
     def _compare_maps(self, map1, map2):
         result = {
